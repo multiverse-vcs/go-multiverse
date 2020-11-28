c61675aab70477e531d9f77aa9c6f3b9ffcdbc35
Keenan Nemetz <keenan.nemetz@gmail.com>
2020-11-28 15:25:34 -0800

    Add fetch cmd.

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -3,23 +3,21 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-merkledag"
 	"github.com/multiverse-vcs/go-multiverse/config"
-	"github.com/multiverse-vcs/go-multiverse/core"
 	"github.com/multiverse-vcs/go-multiverse/p2p"
 	"github.com/multiverse-vcs/go-multiverse/storage"
 	"github.com/urfave/cli/v2"
 )
 
-// NewCloneCommand returns a new command.
-func NewCloneCommand() *cli.Command {
+// NewFetchCommand returns a new command.
+func NewFetchCommand() *cli.Command {
 	return &cli.Command{
-		Name:      "clone",
-		Usage:     "copy an existing repo",
-		ArgsUsage: "<commit-cid> <dir>",
+		Name:      "fetch",
+		Usage:     "copy changes from peers",
+		ArgsUsage: "<commit-cid> <branch-name>",
 		Action: func(c *cli.Context) error {
 			if c.NArg() < 2 {
 				cli.ShowSubcommandHelpAndExit(c, 1)
@@ -35,19 +33,19 @@ func NewCloneCommand() *cli.Command {
 				return cli.Exit(err.Error(), 1)
 			}
 
-			root := filepath.Join(cwd, c.Args().Get(1))
-			if err := os.Mkdir(root, 0755); err != nil {
-				return cli.Exit(err.Error(), 1)
-			}
-
-			dot := filepath.Join(root, storage.DotDir)
-			if err := os.Mkdir(dot, 0755); err != nil {
-				return cli.Exit(err.Error(), 1)
-			}
-
-			store, err := storage.NewOsStore(root)
+			store, err := storage.NewOsStore(cwd)
 			if err != nil {
 				return cli.Exit(err.Error(), 1)
+			}
+
+			cfg, err := store.ReadConfig()
+			if err != nil {
+				return cli.Exit(err.Error(), 1)
+			}
+
+			name := c.Args().Get(1)
+			if b, ok := cfg.Branches[name]; ok && b.Head.Defined() {
+				return cli.Exit("branch exists and is not empty", 1)
 			}
 
 			if err := store.Online(c.Context); err != nil {
@@ -70,14 +68,7 @@ func NewCloneCommand() *cli.Command {
 				return cli.Exit(err.Error(), 1)
 			}
 
-			if err := core.Checkout(c.Context, store, id); err != nil {
-				return cli.Exit(err.Error(), 1)
-			}
-
-			cfg := config.Default()
-			cfg.Index = id
-			cfg.SetHead(id)
-
+			cfg.Branches[name] = &config.Branch{Head: id}
 			if err := store.WriteConfig(cfg); err != nil {
 				return cli.Exit(err.Error(), 1)
 			}
